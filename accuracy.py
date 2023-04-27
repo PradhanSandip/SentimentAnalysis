5ea6431a4957cb0ab98be9fc83e61f0203f50912
mrduck6 <mrduckandduck6@gmail.com>
2023-04-27 10:30:34 +0100

    updated

--- accuracy.py
+++ accuracy.py
@@ -1,7 +1,9 @@
 from ProcessData import ProcessData
-# from roberta import roberta_sentiment
-# from vader import vader
+import roberta
+import vader
 import pandas as pd
+from nltk import word_tokenize
+import matplotlib.pyplot as plt
 '''------------------------This class is used to measure accuracy of various sentiment analysis approach-----------------------'''
 class Accuracy:
     #constructor
@@ -10,63 +12,72 @@ class Accuracy:
         self.model_accuracy_score = 0
         #vader score
         self.vader_accuracy_score = 0
-        '''   
+        self.vader_plot = []
+        self.model_plot = []
+        self.model_accuracy("review_labeled.csv")
+        self.vader_accuracy("review_labeled.csv")
+        
     #function calculates accuracy of machine learning model, with review data
     def model_accuracy(self, csv_file):
+        r = roberta.Roberta()
         #loading the labeled reviews file
-        data = ProcessData(csv_file)
+        data = ProcessData(csv_file, False)
         #list of reviews
-        reviews = data.get_reviews()
+        reviews = data.get_data_frame()[1]
         #list of labels
-        labels = data.get_data_frame()["label"]
+        labels = data.get_data_frame()[2]
         #for each review
         for index, review in enumerate(reviews):
             #perform a inference and store the result
-            output = roberta_sentiment(review)
+            output = r.roberta_sentiment(review)
             #if the output tag matches the label
             if(output[0] == labels[index]):
+                
                 #add 1 score
                 self.model_accuracy_score += 1
+                self.model_plot.append(self.model_accuracy_score)
+            else:
+                self.model_plot.append(self.model_accuracy_score)
 
         #return the result in %
         return (self.model_accuracy_score/len(reviews))*100
-
+  
     #function calculates accuracy of vader sentiment
     def vader_accuracy(self, csv_file):
+        v = vader.Vader()
         #loading the labeled reviews file
-        data = ProcessData(csv_file)
+        data = ProcessData(csv_file,False)
         #list of reviews
-        reviews = data.get_reviews()
+        reviews = data.get_data_frame()[1]
         #list of labels
-        labels = data.get_data_frame()["label"]
+        labels = data.get_data_frame()[2]
         #for each review
         for index, review in enumerate(reviews):
             #perform a inference and store the result
-            output = vader(review)
+            output = v.vader(review)
             #if the output tag matches the label
             if(output[0] == labels[index]):
+                
                 #add 1 score
                 self.vader_accuracy_score += 1
-
+                self.vader_plot.append(self.vader_accuracy_score)
+            else:
+                self.vader_plot.append(self.vader_accuracy_score)
         #return the result in %
         return (self.vader_accuracy_score/len(reviews))*100
 
-'''
-    # def create(self):
-    #     #loading the labeled reviews file
-    #     data = ProcessData("reviews.csv",True)
-    #     #list of reviews
-    #     reviews = data.get_reviews()[1:101]
-    #     frame = {"review":[],"label":[]}
-    #     for review in reviews:
-    #         frame["review"].append(review.replace("\r",""))
-    #         frame["label"].append("test")
-    #     df = pd.DataFrame(frame)
-    #     print(df)
-    #     df.to_csv("review_labeled.csv", sep="\t", line_terminator="\n", header=["review", "label"])
+    def plot_result(self):
+        plt.plot(self.model_plot,label=f"Roberta accuracy {self.model_accuracy_score}%")
+        plt.plot(self.vader_plot,label=f"Vader accuracy {self.vader_accuracy_score}%")
+        plt.legend()
+        plt.show()
+        
 
 
-d = ProcessData("review_labeled.csv", False).get_data_frame()
+
+
+
+
 
 
 
